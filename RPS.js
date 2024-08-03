566663597df4c63a8f843045ba8b34c48dfcdd6a
Ode Millington <odemillington05@gmail.com>
2024-08-03 16:35:56 -0400

    Made UI for Rock Paper Scissors

--- RPS.js
+++ RPS.js
@@ -3,59 +3,104 @@ const options = ["Rock", "Paper", "Scissors"]
 let playerScore = 0
 let computerScore = 0
 
+
+const playerscore = document.querySelector("#player_score")
+const computerscore = document.querySelector("#computer_score")
+
+const playerselection = document.querySelector('#playerselection')
+const result = document.querySelector('#result')
+
+
+const rockbtn = document.querySelector('#rock')
+const paperbtn = document.querySelector('#paper')
+const scissorsbtn = document.querySelector('#scissors')
+
+rockbtn.addEventListener('click', () => {
+    playRound('Rock', getComputerChoice())
+})
+
+paperbtn.addEventListener('click', () => {
+    playRound('Paper', getComputerChoice())
+})
+
+scissorsbtn.addEventListener('click', () => {
+    playRound('Scissors', getComputerChoice())
+})
+
 function getComputerChoice() {
     let computerChoice = options[Math.floor(Math.random() * 3)]
     return computerChoice
 }
 
-function game() {
-
-
-    for (let session = 0; session<5; session++) {
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Rock🪨, Paper📃 or Scissors✂️?");
-
-        playRound(playerSelection, computerSelection)        
-
-    }
-
-    let result = (playerScore == computerScore) ? "Overall Tie" : (playerScore > computerScore) ? "You win!" : "You LOSE!";
-    alert(`**FINAL SCORES**\nComputer: ${computerScore}\nYou: ${playerScore} \n\n${result}`)
-
-}
 
 function playRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) // my intelligent solution to pythons own .capatilize
+
+    if (playerSelection == 'Rock') {
+        playerselection.textContent = '✊'
+     } if (playerSelection == 'Paper') {
+        playerselection.textContent = '✋'
+     } if (playerSelection == 'Scissors') {
+        playerselection.textContent = '✌'
+     }
 
     if ((playerSelection === "Rock" && computerSelection === "Scissors")
     || playerSelection === "Paper" && computerSelection === "Rock" 
     || playerSelection === "Scissors" && computerSelection === "Paper") {
 
-         alert(`Computer Selection: ${computerSelection} \nPlayer Selection: ${playerSelection} \n\nYou win! ${playerSelection} beats ${computerSelection}`)
-
          playerScore += 1;
+         checkScores() 
+         playerscore.textContent = 'Player Score: ' + playerScore
+         result.textContent = `YOU WON ${playerSelection} BEATS ${computerSelection}`
+         result.setAttribute("style", 'color:#6a994e')
+         
     }
+
+    if (computerSelection == 'Rock') {
+        computerselection.textContent = '✊'
+     } if (computerSelection == 'Paper') {
+        computerselection.textContent = '✋'
+     } if (computerSelection == 'Scissors') {
+        computerselection.textContent = '✌'
+     }
 
     if ((computerSelection === "Rock" && playerSelection === "Scissors")
     || computerSelection === "Paper" && playerSelection === "Rock" 
     || computerSelection === "Scissors" && playerSelection === "Paper") {
 
-        alert(`Computer Selection: ${computerSelection} \nPlayer Selection: ${playerSelection}\n\nYou LOSE! ${computerSelection} beats ${playerSelection}`)
-
         computerScore += 1
+        checkScores() 
+        computerscore.textContent = 'Computer Score: ' + computerScore
+        result.textContent = `YOU LOST ${computerSelection} BEATS ${playerSelection}`
+        result.setAttribute("style", 'color:crimson')
+        
     }
 
-    if (computerSelection === playerSelection) {
+    if (computerSelection == playerSelection) {
+        result.textContent = 'DRAW'
+        result.setAttribute("style", 'color:whitesmoke')
 
-        // replays round if draw - no draws allowed here 
-        alert(`Computer Selection: ${computerSelection} \nPlayer Selection: ${playerSelection}\n\nTie. Replay Round!`)
-
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Rock🪨, Paper📃 or Scissors✂️?");
-        playRound(playerSelection, computerSelection)
     }
 
 }
 
-
-game()
+function checkScores() {
+    if (playerScore == 5) {
+        result.textContent = `YOU WON.  CHOOSE AGAIN TO RESTART`;
+        result.setAttribute("style", 'color:#6a994e');
+        playerScore = 0;
+        computerScore = 0;
+        playerscore.textContent = 'Player Score: ' + playerScore;
+        computerscore.textContent = 'Computer Score: ' + computerScore;
+    }
+    
+    if (computerScore == 5) {
+        result.textContent = `YOU LOST.  CHOOSE AGAIN TO RESTART`;
+        result.setAttribute("style", 'color:crimson');
+        playerScore = 0;
+        computerScore = 0;
+        playerscore.textContent = 'Player Score: ' + playerScore;
+        computerscore.textContent = 'Computer Score: ' + computerScore;
+    }
+}
